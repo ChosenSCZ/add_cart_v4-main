30e6bcce92d89d5daa7538ce411272b176f6954d
ChosenSCZ <christoffer.espinozah@gmail.com>
2024-03-30 00:25:05 -0400

    zz

--- checkout.js
+++ checkout.js
@@ -1,8 +1,8 @@
 import products from './products.js';
 const cart = () => {
-    let listCartHTML = document.querySelector('.list');
+    let listCartHTML = document.querySelector('.listCart');
     let iconCart = document.querySelector('.icon-cart');
-    let closeBtn = document.querySelector('.cartTab .close');
+    let closeBtn = document.querySelector('.cartTab2 .close');
     let iconCartSpan = iconCart.querySelector('span');
     let body = document.querySelector('body');
     let closeCart = document.querySelector('.close');
@@ -16,7 +16,7 @@ const cart = () => {
     closeBtn.addEventListener('click', () => {
         body.classList.toggle('activeTabCart');
     })
-
+    
     const setProductInCart = (idProduct, quantity, position) => {
         if(quantity > 0){
             if(position < 0){
@@ -34,7 +34,7 @@ const cart = () => {
         refreshCartHTML();
     }
     const refreshCartHTML = () => {
-        let listHTML = document.querySelector('.returnCart .list');
+        let listHTML = document.querySelector('.listCart');
         let totalHTML = document.querySelector('.icon-cart span');
         let totalQuantity = 0;
         listHTML.innerHTML = null;
@@ -63,8 +63,6 @@ const cart = () => {
         })
         totalHTML.innerText = totalQuantity;
     }   
-
-//EVEN CLICK
     document.addEventListener('click', (event) => {
         let buttonClick = event.target;
         let idProduct = buttonClick.dataset.id;
@@ -80,7 +78,6 @@ const cart = () => {
             setProductInCart(idProduct, quantity, position);
         }
     })
-
     const initApp = () => {
         
         if(localStorage.getItem('cart')){
@@ -92,50 +89,3 @@ const cart = () => {
 initApp();
 }
 export default cart;
-
-/*import products from './products.js';
-let listCart = [];
-function checkCart(){
-        var cookieValue = document.cookie
-        .split('; ')
-        .find(row => row.startsWith('listCart='));
-        if(cookieValue){
-            listCart = JSON.parse(cookieValue.split('=')[1]);
-        }
-}
-checkCart();
-addCartToHTML();
-function addCartToHTML(){
-    // clear data default
-    let listCartHTML = document.querySelector('.returnCart .list');
-    listCartHTML.innerHTML = null;
-
-    let totalQuantityHTML = document.querySelector('.totalQuantity');
-    let totalPriceHTML = document.querySelector('.totalPrice');
-    let totalQuantity = 0;
-    let totalPrice = 0;
-    // if has product in Cart
-    if(listCart){
-        listCart.forEach(item => {
-            if(item){
-                let info = products[position];
-                let newITem = document.createElement('div');
-                newITem.classList.add('item');
-                newITem.innerHTML = 
-                    `<img src="${info.image}">
-                    <div class="info">
-                        <div class="name">${info.name}</div>
-                        <div class="price">Bs${info.price}/1 product</div>
-                    </div>
-                    <div class="quantity">
-                    <span class="minus" data-id="${info.id}">-</span>
-                    <span>${item.quantity}</span>
-                    <span class="plus" data-id="${info.id}">+</span>
-                    </div>
-                    <div class="returnPrice">$${info.price * item.quantity}</div>`;
-                listCartHTML.appendChild(newCart);
-                totalQuantity = totalQuantity + item.quantity;
-                totalPrice = totalPrice + (info.price * item.quantity);
-            }
-        })
-    }*/
